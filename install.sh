48509c64fa5d6860c916fb4e703eff4220fe2517
joe miller <joeym@joeym.net>
2012-02-29 19:49:15 -0700

    expanded tabs to 4 whitespace for consistency

--- install.sh
+++ install.sh
@@ -9,29 +9,29 @@ SRC_FILES="git-flux-checkout git-flux-env git-flux-feature \
 git-flux-init git-flux-publish git-flux-up gitflux-common"
 
 case "$1" in
-	install)
-		echo "Installing $NAME to $INSTALL_PATH"
-		install -v -d -m 0755 "$INSTALL_PATH"
-		for file in $EXEC_FILES ; do
-			install -v -m 0755 "$WHEREAMI/$file" "$INSTALL_PATH"
-		done
-		for file in $SRC_FILES ; do
-			install -v -m 0644 "$WHEREAMI/$file" "$INSTALL_PATH"
-		done		
-		exit
-		;;
-	uninstall)
-		echo "Uninstalling $NAME from $INSTALL_PATH"
-		for file in $EXEC_FILES $SRC_FILES ; do
-			echo "rm -f $INSTALL_PATH/$file"
-			rm -f "$INSTALL_PATH/$file"
-		done
-		exit
-		;;
-	*)
-		echo "Usage: [environment] $0 [install|uninstall]"
-		echo "Environment :"
-		echo "   INSTALL_PATH=$INSTALL_PATH"
-		exit
-		;;
+    install)
+        echo "Installing $NAME to $INSTALL_PATH"
+        install -v -d -m 0755 "$INSTALL_PATH"
+        for file in $EXEC_FILES ; do
+            install -v -m 0755 "$WHEREAMI/$file" "$INSTALL_PATH"
+        done
+        for file in $SRC_FILES ; do
+            install -v -m 0644 "$WHEREAMI/$file" "$INSTALL_PATH"
+        done        
+        exit
+        ;;
+    uninstall)
+        echo "Uninstalling $NAME from $INSTALL_PATH"
+        for file in $EXEC_FILES $SRC_FILES ; do
+            echo "rm -f $INSTALL_PATH/$file"
+            rm -f "$INSTALL_PATH/$file"
+        done
+        exit
+        ;;
+    *)
+        echo "Usage: [environment] $0 [install|uninstall]"
+        echo "Environment :"
+        echo "   INSTALL_PATH=$INSTALL_PATH"
+        exit
+        ;;
 esac
